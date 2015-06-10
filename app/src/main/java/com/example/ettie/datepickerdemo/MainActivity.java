da6fc5c56ddf55536617fb7175e209c10567f240
Schreiby <ettie.schreiber@gmail.com>
2015-06-10 14:28:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ettie/datepickerdemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ettie.datepickerdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private DatePicker datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datePicker = (DatePicker)findViewById(R.id.datePicker1);
+    }
+
+    public void showDate(View view) {
+        int month = datePicker.getMonth();
+        int day = datePicker.getDayOfMonth();
+        int year = datePicker.getYear();
+
+        Toast.makeText(this, "Date Selected: " + month + "/" + day + "/" + year, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
